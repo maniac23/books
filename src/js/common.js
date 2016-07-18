1113b928abba85f9cd012b5bf47fcbac173cd2ee
Mikhail Tukai <tukaym@gmail.com>
2016-07-18 23:37:57 +0300

    book block

--- src/js/common.js
+++ src/js/common.js
@@ -1,1 +1,8 @@
-'use strict'
+$(document).ready(function() {
+  $('.main-header').backgroundBlur({
+    imageURL: '../img/book-cover.jpg',
+    blurAmount: 40,
+    imageClass: 'main-header__background',
+    endOpacity: 0.3
+  });
+});
